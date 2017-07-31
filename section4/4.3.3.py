bdeda42208214f57d468719d62ee749ec626f1aa
s-tajima <tajima1989@gmail.com>
2017-07-31 20:21:28 +0900

    fix 4.3.3

--- section4/4.3.3.py
+++ section4/4.3.3.py
@@ -2,19 +2,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import sys, os
 from common import lib as l
 from common import mnist as m
-import pickle
-
-def numerical_diff(f, x):
-    h = 1e-4
-    return (f(x+h) - f(x-h)) / (2*h)
 
 def function_2(x):
     return x[0]**2 + x[1]**2
-
-def tangent_line(f, x):
-    d = numerical_diff(f, x)
-    y = f(x) - d*x
-    return lambda t: d*t + y
 
 x1 = l.np.arange(-3.0, 3.0, 0.1)
 x2 = l.np.arange(-3.0, 3.0, 0.1)
@@ -27,6 +17,6 @@ l.plt.ylabel("f(x)")
 
 fig = l.plt.figure()
 ax = Axes3D(fig)
-ax.plot_wireframe(X1,X2,y) #<---ここでplot
+ax.plot_wireframe(X1,X2,y)
 
 l.plt_show_alt(l.plt, "4.3.3")
